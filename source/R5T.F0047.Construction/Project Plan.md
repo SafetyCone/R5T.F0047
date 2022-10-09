cc147f113420f3d61532424d68760381352d36df
davidcoats <david@getsafetycone.com>
2022-10-08 21:24:09 -0700

    Initial commit.

---
+++ source/R5T.F0047.Construction/Project Plan.md
@@ -0,0 +1,2 @@
+# R5T.F0047.Construction
+Construction console project for the R5T.F0047 library.
---
+++ source/R5T.F0047/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.F0047
+{
+	/// <summary>
+	/// GitHub owner functionality.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
